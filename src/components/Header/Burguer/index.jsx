052fd676c4bce9221d61de671c9e88b042f994d4
Pedro Henrique Pires do Nascimento <dckpedro@gmail.com>
2023-11-01 13:30:09 -0300

    style: componentizando logos svg

---
+++ src/components/Header/Burguer/index.jsx
@@ -0,0 +1,5 @@
+export default function Burguer() {
+  return (
+    <div>Burguer</div>
+  )
+}
---
+++ src/components/Header/index.jsx
@@ -0,0 +1,13 @@
+import Burguer from "./Burguer"
+import LogoDefault from "../LogoDefault"
+
+
+export default function Header() {
+  return (
+    <div>
+      <LogoDefault />
+      <p>Pedro</p>
+      <Burguer />
+    </div>
+  )
+}
