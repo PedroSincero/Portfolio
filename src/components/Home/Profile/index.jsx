00de9ead02b5a65b62bd03141940b00351f239b4
Pedro Henrique Pires do Nascimento <dckpedro@gmail.com>
2023-11-08 16:10:49 -0300

    adicionando dot

--- src/components/Home/Profile/index.jsx
+++ src/components/Home/Profile/index.jsx
@@ -1,10 +1,18 @@
 import ImageDefault from '../../../assets/ImageDefault.png';
+import Logo from '../../../assets/Logo.png';
+import Dots from '../../../assets/Dots.png';
 
 export default function Profile() {
   return (
     <div className=" flex flex-col items-center flex-grow">
-      <div >
-          <img src={ImageDefault} alt="default Image" />
+      <div>
+        <div className='relative'>
+          <img className='absolute right-0 -bottom-80 z-10' src={Dots} alt="Dots" />
+        </div>
+        <div className='relative'>
+          <img className='absolute -z-10' src={Logo} alt="Logo" />
+        </div>
+        <img src={ImageDefault} alt="default Image" />
       </div>
         <div className=" p-2 items-center border border-secondary">
           <p className=" text-lg text-secondary"><span className="text-primary"># </span>Em Busca de novas <span className="text-white font-bold">Oportunidades</span></p>
