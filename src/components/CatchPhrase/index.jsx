c4f28ce9c7155842a86056b863dd5d4bb35ca112
Pedro Henrique Pires do Nascimento <dckpedro@gmail.com>
2023-11-09 17:23:52 -0300

    style: ocultando frase na versao mobile

--- src/components/CatchPhrase/index.jsx
+++ src/components/CatchPhrase/index.jsx
@@ -2,7 +2,7 @@ import Quotes from '../../assets/Quotes.svg';
 
 export default function CatchPhrase({ phase }) {
   return (
-    <div className=" flex flex-col self-center text-center">
+    <div className=" hidden lg:flex flex-col self-center text-center">
       <div className=" inline-block mt-10 p-8 gap-3 border border-secondary text-2xl font-bold">
         <div className='relative'>
           <img className='absolute bottom-4 -left-3 bg-background p-1' src={Quotes} alt="" />
