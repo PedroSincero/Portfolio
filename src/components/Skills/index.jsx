5645b8f4b1f91a14fae561ba791103784243298b
Pedro Henrique Pires do Nascimento <dckpedro@gmail.com>
2023-11-20 16:45:16 -0300

    finalizando skills

--- src/components/Skills/index.jsx
+++ src/components/Skills/index.jsx
@@ -7,15 +7,23 @@ export default function Skills() {
     <>
       <div className="hidden lg:flex pb-12">
         <p className='text-4xl font-bold'><span className='text-primary'>#</span>skills</p>
-        <img className=' w-4/5 px-5 pr-20' src={Line} alt="Line" />
+        <img className=' w-2/4 px-5 pr-20' src={Line} alt="Line" />
       </div>
       <div className='flex'>
-      <img src={SkillPhoto} alt="teste" />
-      <Skill title="Languages" tecnologies={['Typescript ', 'Lua ', 'Python ', 'Javascript ']}/>
-      <Skill title="Databases" tecnologies={['SQLite ', 'PostgreSQL ', 'Mongo ']}/>
-      <Skill title="Tools" tecnologies={['VSCode', 'Neovim ', 'Linux ', 'Figma ', 'XFCE', 'Arch', 'Git', 'Font', 'Awesome']}/>
-      <Skill title="Other" tecnologies={['HTML ', 'CSS ', 'EJS ', 'SCSS ', 'REST', 'Jinja']}/>
-      <Skill title="Frameworks" tecnologies={['React ', 'Vue ', 'Disnake ', 'Discord ', 'Flask', 'Express.js']}/>
+        <div className=' w-2/5 ml-72'>
+          <img className=' w-full' src={SkillPhoto} alt="teste" />
+        </div>
+        <div className=' grid grow justify-end content-start'>
+          <div className=' flex flex-row gap-3 pb-4'>
+            <Skill title="Languages" tecnologies={['Typescript ', 'Lua ', 'Python ', 'Javascript ']}/>
+            <Skill title="Databases" tecnologies={['SQLite ', 'PostgreSQL ', 'Mongo ']}/>
+            <Skill title="Tools" tecnologies={['VSCode', 'Neovim ', 'Linux ', 'Figma ', 'XFCE ', 'Arch ', 'Git ', 'Font ', 'Awesome ']}/>
+          </div>
+        <div className=' flex justify-end gap-3'>
+          <Skill title="Other" tecnologies={['HTML ', 'CSS ', 'EJS ', 'SCSS ', 'REST', 'Jinja']}/>
+          <Skill title="Frameworks" tecnologies={['React ', 'Vue ', 'Disnake ', 'Discord ', 'Flask', 'Express.js']}/>
+        </div>
+        </div>
       </div>
     </>
   )
