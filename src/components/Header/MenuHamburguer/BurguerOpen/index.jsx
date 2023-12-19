8917243358f34a152541738d9f4e086abf15a71c
Pedro Henrique Pires do Nascimento <dckpedro@gmail.com>
2023-12-19 16:16:22 -0300

    corrigindo bugs

--- src/components/Header/MenuHamburguer/BurguerOpen/index.jsx
+++ src/components/Header/MenuHamburguer/BurguerOpen/index.jsx
@@ -1,6 +1,6 @@
 export default function BurguerOpen() {
   return (
-    <div className="absolute w-full h-full left-0 top-20 z-20 bg-background">
+    <div className="absolute h-screen w-screen left-0 top-20 z-50 bg-background">
       <div className="flex flex-col gap-8 text-5xl mt-10 p-4">
         <a href="#"><span className=" text-primary">#</span>home</a>
         <a href="#"><span className=" text-primary">#</span>works</a>
