5a4a2b78aa73bb5a8425b5a01368e42bc7f0f3ca
Pedro Henrique Pires do Nascimento <dckpedro@gmail.com>
2023-12-19 16:43:26 -0300

    configurando o router dom

--- src/main.jsx
+++ src/main.jsx
@@ -2,4 +2,28 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
-ReactDOM.createRoot(document.getElementById('root')).render(<App />)
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
+import { About,
+  Contacts,
+  Projects,
+  Skills } from './pages';
+
+const router = createBrowserRouter([{
+  path: '/',
+  element: <App />,
+}, {
+  path: 'projects',
+  element: <Projects />,
+}, {
+  path: 'skills',
+  element: <Skills />,
+}, {
+  path: 'about',
+  element: <About />,
+}, {
+  path: 'contact',
+  element: <Contacts />,
+}]);
+
+ReactDOM.createRoot(document.getElementById('root')).render(<RouterProvider router={router} />)
