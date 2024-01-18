0d5d1643dea2f04da303353f811e67d5bb8fee01
Pedro Henrique Pires do Nascimento <dckpedro@gmail.com>
2024-01-18 14:25:46 -0300

    adicionnado e estilizando funfacts

--- src/pages/About/index.jsx
+++ src/pages/About/index.jsx
@@ -32,7 +32,16 @@ export default function About() {
           </div>
         </div>
         <div>
-          <Title title={'fun-facts'} span={'#'}/>
+          <Title title={'my-fun-facts'} span={'#'}/>
+          <div className=" inline-flex flex-wrap gap-3 w-3/5">
+            <p className="border border-secondary p-2 text-secondary">I like more than summer</p>
+            <p className="border border-secondary p-2 text-secondary">I often bike with my friends</p>
+            <p className="border border-secondary p-2 text-secondary">I like <strong>Pizza</strong> and <strong>Pasta</strong> </p>
+            <p className="border border-secondary p-2 text-secondary">I was in <strong>Egypt</strong>, <strong>Poland</strong> and <strong>Turkey</strong></p>
+            <p className="border border-secondary p-2 text-secondary">My favorite movie is <strong>The Green Mile</strong></p>
+            <p className="border border-secondary p-2 text-secondary">I am still in school</p>
+            <p className="border border-secondary p-2 text-secondary">I don`t have any siblings</p>
+          </div>
         </div>
       </div>
       <Footer />
