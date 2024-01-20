41356b2b215b538f48db46dcf3dc228215a12f9b
Pedro Henrique Pires do Nascimento <dckpedro@gmail.com>
2024-01-20 14:34:43 -0300

    iniciando pagina de contato

--- src/pages/Contacts/index.jsx
+++ src/pages/Contacts/index.jsx
@@ -1,7 +1,22 @@
-import React from 'react'
+import Header from '../../components/Header'
+import Footer from '../../components/Footer'
+import Title from '../../components/Title'
 
 export default function Contacts() {
+  const text  = `I’m interested in freelance opportunities. However, if you have other request or question, don’t hesitate to contact me`
   return (
-    <div>Contacts</div>
+    <div>
+      <Header />
+        <div className='py-8  px-32 min-[1920px]:px-48'>
+          <Title title={'contacts'} span={'/'} subTitle={'Who am i?'}/>
+          <div className="flex justify-between">
+            <div className="w-1/5 text-secondary pt-16">
+              { text }
+            </div>
+          </div>
+          <Title title={'all-media'} span={'#'}/>
+        </div>
+      <Footer />
+    </div>
   )
 }
