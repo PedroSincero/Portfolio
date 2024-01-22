2d8c6b20a0fcae3c90096ab70a94a9c9d0bcd2dd
Pedro Henrique Pires do Nascimento <dckpedro@gmail.com>
2024-01-22 17:27:01 -0300

    criando parte do contato

--- src/pages/Contacts/index.jsx
+++ src/pages/Contacts/index.jsx
@@ -1,6 +1,8 @@
 import Header from '../../components/Header'
 import Footer from '../../components/Footer'
 import Title from '../../components/Title'
+import Linkedin from '../../assets/Linkedin.svg';
+import Email from '../../assets/Email.svg';
 
 export default function Contacts() {
   const text  = `I’m interested in freelance opportunities. However, if you have other request or question, don’t hesitate to contact me`
@@ -13,8 +15,18 @@ export default function Contacts() {
             <div className="w-1/5 text-secondary pt-16">
               { text }
             </div>
+            <div className=' inline-flex p-4 flex-col items-start gap-4 border border-secondary'>
+              <p>Me mande uma mensagem</p>
+              <div className='flex items-center gap-1'>
+                <img src={Linkedin} alt="Linkedin Logo"  />
+                <p>/in/pedro-henrique-pires-do-nascimento/</p>
+              </div>
+              <div className='flex items-center gap-1'>
+                <img src={Email} alt="Email Logo"  />
+                <p>dckpedro@gmail.com</p>
+              </div>
+            </div>
           </div>
-          <Title title={'all-media'} span={'#'}/>
         </div>
       <Footer />
     </div>
