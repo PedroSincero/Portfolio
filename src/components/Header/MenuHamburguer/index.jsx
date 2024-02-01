15e18a9a56a994bdf1b30b9a8692453613b1cb84
Pedro Henrique Pires do Nascimento <dckpedro@gmail.com>
2024-02-01 16:22:25 -0300

    adicionando efeito hamburguer

--- src/components/Header/MenuHamburguer/index.jsx
+++ src/components/Header/MenuHamburguer/index.jsx
@@ -1,16 +1,16 @@
-import OpenIcon from "./OpenIcon";
-import CloseIcon from "./CloseIcon";
 import { useState } from "react";
+import { Cross as Hamburger } from 'hamburger-react'
 import BurguerOpen from "./BurguerOpen";
 
 export default function MenuHamburguer() {
-  const [open, setOpen] = useState(false);
+  const [isOpen, setOpen] = useState(false);
   return (
     <div className="flex self-center lg:hidden">
-      <button type="button" onClick={() => setOpen(!open)}>
+      {/* <button type="button" onClick={() => setOpen(!open)}>
         {open ? <CloseIcon /> : <OpenIcon setOpen={ setOpen }/> }
-      </button>
-      {open && <BurguerOpen /> }
+      </button> */}
+      <Hamburger toggled={isOpen} toggle={setOpen} />
+      {isOpen && <BurguerOpen setOpen={ setOpen } /> }
     </div>
   )
 }
