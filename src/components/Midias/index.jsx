eaa48127b763ae61fc638df6d91ca5f7440a4146
Pedro Henrique Pires do Nascimento <dckpedro@gmail.com>
2024-02-01 16:43:45 -0300

    ajustando midias link

--- src/components/Midias/index.jsx
+++ src/components/Midias/index.jsx
@@ -1,15 +1,20 @@
-import Line from './Line'
-import Github from './Github'
-import Linkedin from './Linkedin'
-import Discord from './Discord'
+import Line from "./Line";
+import Github from "./Github";
+import Linkedin from "./Linkedin";
 
 export default function Midias() {
   return (
-    <div className=" hidden lg:inline-flex items-center flex-col fixed left-9 z-50 top-0" >
+    <div className=" hidden lg:inline-flex items-center flex-col fixed left-9 z-50 top-0">
       <Line />
-      <Github />
-      <Linkedin />
-      <Discord />
+      <a href="https://github.com/PedroSincero" target="__blank">
+        <Github />
+      </a>
+      <a
+        href="https://www.linkedin.com/in/pedro-henrique-pires-do-nascimento/"
+        target="__blank"
+      >
+        <Linkedin />
+      </a>
     </div>
-  )
+  );
 }
